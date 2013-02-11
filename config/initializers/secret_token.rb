e211028512dbcfc1a80fb9145fe9ef14e4b610ac
unknown <matej.skrabanek@gmail.com>
2013-02-11 19:12:08 +0100

    test2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netstorm::Application.config.secret_token = '0f1ff5e43d7b61fc096c47d7e061928e7a890c568a4589621d46733f810c02f4ec2247bd6b108ae7003b2a94ce5c2062c9a9bb62466b4b853c06b93c1f0060d0'
