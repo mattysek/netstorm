64415578f7571bc38a8327e7505e2947997acc22
unknown <matej.skrabanek@gmail.com>
2013-02-13 23:58:23 +0100

    current user tracking v0.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,22 +7,24 @@ class SessionsController < ApplicationController
     if session[:user_id]
       # Means our user is signed in. Add the authorization to the user
       User.find(session[:user_id]).add_provider(auth_hash)
-      render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+      # render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+	  redirect_to '/'
     else
       # Log him in or sign him up
       auth = Authorization.find_or_create(auth_hash)
       # Create the session
       session[:user_id] = auth.user.id
-      render :text => "Welcome #{auth.user.name}!"
+      # render :text => "Welcome #{auth.user.name}!"
+	  redirect_to '/'
     end
   end
 
   def failure
-    render :text => "Sorry, but you didn't allow access to our app!"
+    redirect_to '/'
   end
 
   def destroy
     session[:user_id] = nil
-    render :text => "You've logged out!"
+    redirect_to '/'
   end
 end
