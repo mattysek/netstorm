111a08026b5a688696f45aa5da585757144b98bf
unknown <matej.skrabanek@gmail.com>
2013-02-15 16:35:01 +0100

    canvas updates

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,10 @@
 class RoomsController < ApplicationController
   def index
   	@chat_messages = Message.where('channel = ? AND action = \'chat\'', params[:channel_name])
-	@drawImage_messages = Message.where('channel = ? AND action = \'drawIdea\'', params[:channel_name])
+	@save_message = Message.where('channel = ? AND action = \'save\'', params[:channel_name]).order("created_at").last
+	if not @save_message
+		@drawImage_messages = Message.where('channel = ? AND action = \'drawIdea\'', params[:channel_name])
+	end
   end
 
   def create_message
